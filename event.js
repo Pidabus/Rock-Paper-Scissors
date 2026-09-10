6816a0e97a63012b2dd5e651b4b23327eba51990
Pidabus <affiqfirdaus67@gmail.com>
2026-09-10 12:15:30 +0800

    mlaksjfiowajfoeiwjfoeiqhf;o24017259-fh8340ty74830gt29gt8230

--- event.js
+++ event.js
@@ -1,6 +1,13 @@
 // const btn = document.querySelector("#btn");
 // btn.onclick = () => alert("Hello, World!");
 
-const btn = document.querySelector("#btn");
-btn.addEventListener("click", () => {alert("Hello, World!");});
+function alertFunction() {
+    alert("YAY, YOU DID IT!");
+}
 
+const btn = document.querySelector("#btn");
+// btn.addEventListener("click", alertFunction);
+btn.addEventListener("click", function (e){
+    // console.log(e.target);
+    e.target.style.background = "blue";
+});

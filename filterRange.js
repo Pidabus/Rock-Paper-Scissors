463cc6246d2b26cecea1db7485e4c88aba5b1f26
Pidabus <affiqfirdaus67@gmail.com>
2026-09-11 09:04:28 +0800

    completed filterRange.js

--- filterRange.js
+++ filterRange.js
@@ -1,9 +1,10 @@
-function filterRange(array, a, b) {
-    return array
-        .filter(num => num >= a)
-        .filter(num => num <= b)
+function filterRange(arr, a, b) {
+    return arr
+        .filter(arr => arr >= a)  // .filter(arr >= a) --> This is wrong, it has to be a function.
+        .filter(arr => arr <= b)  // Rememeber! --> These array methods take callback *functions* !!
 }
 
-let arr = [5, 3, 8, 1];
-let filteredArr = filterRange(arr, 2, 7);
-console.log(filteredArr);
+let array = [5, 3, 8, 1];
+
+const filtered = filterRange(array, 2, 7);
+console.log(filtered);

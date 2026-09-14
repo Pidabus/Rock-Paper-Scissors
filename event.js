791e19c96d94f6ddfaa6f409366715300c61d2d4
Pidabus <affiqfirdaus67@gmail.com>
2026-09-14 11:34:22 +0800

    buttons DOM event etc.

--- event.js
+++ event.js
@@ -1,14 +1,5 @@
-const btn = document.querySelector("button");
+const buttons = document.querySelectorAll("button");
 
-function random(number) {
-  return Math.floor(Math.random() * (number));
-}
-
-function bgChange(e) {
-  const rndCol = `rgb(${random(256)}, ${random(256)}, ${random(256)})`;
-  e.target.style.backgroundColor = rndCol;
-  console.log(rndCol);
-  console.log(e);
-}
-
-btn.addEventListener("click", bgChange);
+buttons.forEach(button => {
+    button.addEventListener("click", e => console.log(e.target))
+});

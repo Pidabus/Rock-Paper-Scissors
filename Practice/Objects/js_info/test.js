4dd2e7e8ae7a51817a4ce24b315861581673d9c6
Pidabus <affiqfirdaus67@gmail.com>
2026-09-18 15:26:57 +0800

    completed create keyed objects from array

--- Practice/Objects/js_info/test.js
+++ Practice/Objects/js_info/test.js
@@ -1,7 +1,5 @@
-let users = [
-    { id: 'john', name: "John Smith", age: 20 },
-    { id: 'ann', name: "Ann Smith", age: 24 },
-    { id: 'pete', name: "Pete Peterson", age: 31 },
-];
+obj = {};
 
-console.log(users[0].id);
+obj.id = 22;
+
+console.log(obj);
